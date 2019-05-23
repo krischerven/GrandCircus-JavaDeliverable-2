d331824d5d818db6d063a11a97617e4b70b4c3fc
Kris Cherven <krischerven@gmail.com>
2019-05-23 04:39:33 -0400

    Minor changes

--- src/unicodecalculator/UnicodeCalculator.java
+++ src/unicodecalculator/UnicodeCalculator.java
@@ -6,21 +6,28 @@ public class UnicodeCalculator {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Input the first string:");
-		var s1 = s.nextLine();
+		String s1 = s.nextLine();
 		System.out.println("Input the second string:");
-		var s2 = s.nextLine();
+		String s2 = s.nextLine();
 		s.close();
+
+		System.out.println("************************");
 
 		var r1 = 0;
 		for (int i = 0; i < s1.length(); i++) {
-			r1 += s1.charAt(i);
+			char x = s1.charAt(i);
+			System.out.println(x+": "+(int)x);
+			r1 += x;
 		}
 
 		var r2 = 0;
 		for (int i = 0; i < s2.length(); i++) {
-			r2 += s2.charAt(i);
+			char x = s2.charAt(i);
+			System.out.println(x+": "+(int)x);
+			r2 += x;
 		}
-
-	    System.out.println("Absolute difference between the unicode values of each string: " + Math.abs(r2-r1));
+		
+		System.out.println("************************");
+		System.out.println("Absolute difference between the unicode values of each string: " + Math.abs(r2-r1));
 	}
 }
